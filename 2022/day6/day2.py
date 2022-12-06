fbaf77669063d6f67f812dcbd7d2dd6de9ddae5f
Halit Anil Donmez <halitanil.donmez@gmail.com>
2022-12-06 06:18:25 +0100

    Add files via upload

---
+++ 2022/day6/day2.py
@@ -0,0 +1,27 @@
+import re
+import collections
+
+T = "bb.txt"
+M = "aa.txt"
+
+file1 = open(M, 'r')
+Lines = file1.read().splitlines()
+
+for line in Lines: 
+    if line:
+        s = []
+        for i in range(0, len(line)):
+            s.append(line[i])
+            for j in range(i + 1, len(line)):
+                if len(s) == 14:
+                    print('found', j)
+                    break
+                if line[j] not in s:
+                    s.append(line[j])
+                else:
+                    s = []
+                    break
+            if len(s) == 14:
+                break
+    else:
+        print('')
